e06fffc9d5c84eb2dc5aaa451d5ce255998492f7
Darin Webb <dwebb@sourceallies.com>
2018-02-16 08:23:50 -0600

    linting

--- layouts/ihssa.vue
+++ layouts/ihssa.vue
@@ -8,13 +8,13 @@
 </script>
 
 <style lang="scss">
-@import '../node_modules/turretcss/dist/turretcss.min.css';
+@import "../node_modules/turretcss/dist/turretcss.min.css";
 
 body {
-    background: #ffcc33;
+  background: #ffcc33;
 }
 
 main {
-    margin-top: 1em;
+  margin-top: 1em;
 }
 </style>
--- pages/ihssa/index.vue
+++ pages/ihssa/index.vue
@@ -1,26 +1,25 @@
 <template>
-<div class="container">
+  <div class="container">
     <header>
-        <h1 class="">IHSSA All-State Contest</h1>
-        <p class="lead">West Des Moines Valley High School</p>
+      <h1 class="">IHSSA All-State Contest</h1>
+      <p class="lead">West Des Moines Valley High School</p>
     </header>
     <main>
-        <event-list></event-list>
+      <event-list/>
     </main>
-</div>
+  </div>
 </template>
 
 <script>
-import EventList from './event-list'
+import EventList from "./event-list"
 export default {
-    layout: 'ihssa',
-    components: { EventList }
+  layout: "ihssa",
+  components: { EventList }
 }
 </script>
 
 <style lang="scss">
 .container {
-    margin-top: 1em;
+  margin-top: 1em;
 }
-
 </style>
