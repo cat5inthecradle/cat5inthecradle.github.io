4faf65892cdf3b3f9fd744a0a59f3ff6da0de761
Darin Webb <dwebb@sourceallies.com>
2018-02-16 01:05:16 -0600

    use nuxt instead

---
+++ layouts/ihssa.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>
+        <h1>IHSSA</h1>
+    </header>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+@import '../node_modules/turretcss/dist/turretcss.min.css';
+</style>
